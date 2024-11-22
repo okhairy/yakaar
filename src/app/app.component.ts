a9b74cce228de4753f4471333ae2b719eca8fe96
okhairy <oumoulkhairyndiaye1.com@gmail.com>
2024-11-22 09:23:45 +0000

    recuperation du travail de thierno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardClientComponent } from './dashboard-client/dashboard-client.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  //imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [DashboardClientComponent], // Ajoutez le Dashboard ici
 })
 export class AppComponent {
   title = 'yakaar';
+  showDashboard = true; // Passez à "true" pour afficher le Dashboard
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { DashboardClientComponent } from './app/dashboard-client/dashboard-client.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(DashboardClientComponent);
