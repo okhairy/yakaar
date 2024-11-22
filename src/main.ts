7f79cfd7f5f3f0e826fc93e8ee809764527cf7c4
kinababacarsy <fatoukinediakite717@gmail.com>
2024-11-22 15:44:35 +0000

    mis à jour

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { DashboardClientComponent } from './app/dashboard-client/dashboard-client.component';
 
-<<<<<<< HEAD
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
-=======
+
 bootstrapApplication(DashboardClientComponent);
->>>>>>> 467e170cae2820e31dcc07b825dbf8b32b6ed504
