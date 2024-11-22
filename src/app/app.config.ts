aaf84928c5979742508d4548159d7b1b6f39512d
Thierno Ngom <tngom1010@gmail.com>
2024-11-22 09:06:40 +0000

    API complet et authentifcation(sans Keypad)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';  // Import de withFetch
 import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom(HttpClientModule),  // Ajouter HttpClientModule ici
+    provideHttpClient(withFetch()),          // Ajouter withFetch() pour activer l'utilisation de fetch
+    provideRouter(routes),
+    provideClientHydration()
+  ]
 };
